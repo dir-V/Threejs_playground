ce4a09f62aa8bd606a090783850d1b57942a1b79
Vincent Adeola <vince.adeola@outlook.com>
2024-03-30 14:15:14 +0000

    removing redunant comments

--- main.js
+++ main.js
@@ -26,7 +26,6 @@ const sizes = {
 }
 
 //Light
-
 const light1 = new THREE.PointLight(0xffffff, 500, 0);
 light1.position.set(0, 0, -10);
 scene.add(light1);
@@ -61,8 +60,6 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.enablePan = false;
 controls.enableZoom = false;
-// controls.autoRotate = true;
-// controls.autoRotateSpeed = 5;
 
 
 
@@ -108,25 +105,12 @@ window.addEventListener("mousemove", (e) => {
     }
 })
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-// camera.position.z = 5;
-
+//Spinning animation
 function animate() {
 	requestAnimationFrame( animate, );
 
-	mesh.rotation.x += 0.003;
-	mesh.rotation.y += 0.003;
+	mesh.rotation.x += 0.005;
+	mesh.rotation.y += 0.005;
 
 	renderer.render( scene, camera );
 }
